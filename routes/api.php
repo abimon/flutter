ebb0f6c9b6c6aa80c4f617f9d7b5a48263a3a4bc
abimon <eddyabby59@gmail.com>
2024-01-25 19:14:02 +0300

    avatar

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,7 @@ Route::controller(UserController::class)->prefix('/user/')->group(function () {
     Route::get('show','show');
     Route::post('login','index');
     Route::post('create','create');
-    Route::post('avatar/','store');
+    Route::post('avatar','store');
 });
 Route::controller(VideoController::class)->prefix('/video/')->group(function (){
     Route::post('upload','create');
