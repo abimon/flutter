921c47b2ac0d77a79b9084fb10df8abd03cf9ffd
abimon <eddyabby59@gmail.com>
2024-07-03 12:47:32 +0300

    post

--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,4 @@ use Illuminate\Support\Facades\Route;
 //         Route::get('');
 //     });
 // });
-Route::post('/esp32', [APIContoller::class,'index']);
+Route::post('/', [APIContoller::class,'index']);
