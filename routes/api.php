11eae97d454a45509e545bdeefb1909da58e7871
abimon <eddyabby59@gmail.com>
2024-07-03 11:29:51 +0300

    post

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,11 @@
 <?php
 
 use App\Http\Controllers\APIContoller;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::controller(APIContoller::class)->group(function () {
-        Route::get('');
-    });
-});
+// Route::middleware('auth:sanctum')->group(function () {
+//     Route::controller(APIContoller::class)->group(function () {
+//         Route::get('');
+//     });
+// });
+Route::post('/esp32', [APIContoller::class,'index']);
