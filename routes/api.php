927dfedcd157dc5bd46931d7f0b6e8ae9ed2c390
abimon <eddyabby59@gmail.com>
2024-07-12 12:02:45 +0300

    migrate

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Http\Controllers\DeviceController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::resources([
+        'user'=>UserController::class,
         'device'=>DeviceController::class,
     ]);
 });
