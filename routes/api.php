c0569cc3d064d083d904cea9bfdbfeebe5ab991c
abimon <eddyabby59@gmail.com>
2024-07-14 17:42:25 +0300

    return user on login

--- routes/api.php
+++ routes/api.php
@@ -7,9 +7,10 @@ use Illuminate\Support\Facades\Route;
 Route::controller(UserController::class)->prefix('/user')->group(function () {
     Route::post('/login', 'create');
     Route::post('/register', 'store');
-    Route::get('/show/{id}','show');
-    Route::put('/update/{id}', 'update');
-    Route::delete('/delete/{id}', 'destroy');
+    Route::get('/show/{id}','show')->middleware('auth:sanctum');
+    Route::get('/get','index')->middleware('auth:sanctum');
+    Route::put('/update/{id}', 'update')->middleware('auth:sanctum');
+    Route::delete('/delete/{id}', 'destroy')->middleware('auth:sanctum');
 });
 Route::middleware('auth:sanctum')->group(function () {
     Route::controller(DeviceController::class)->prefix('/device')->group(function () {
