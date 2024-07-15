bc7d415db8bfaa7fb532b1427dc8a848b64033d6
abimon <eddyabby59@gmail.com>
2024-07-15 11:09:06 +0300

    get device(s)

--- app/Models/Device.php
+++ app/Models/Device.php
@@ -17,6 +17,6 @@ class Device extends Model
         'isOn'
     ];
     public function owner(){
-        return $this->belongsTo(User::class, 'id', 'user_id');
+        return $this->belongsTo(User::class, 'user_id', 'id');
     }
 }
