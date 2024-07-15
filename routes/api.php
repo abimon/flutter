2531f479bf0401950e7a67aaf5d103267ff46e83
abimon <eddyabby59@gmail.com>
2024-07-15 17:17:00 +0300

    get device

--- routes/api.php
+++ routes/api.php
@@ -13,14 +13,12 @@ Route::controller(UserController::class)->prefix('/user')->group(function () {
     Route::put('/update/{id}', 'update')->middleware('auth:sanctum');
     Route::delete('/delete/{id}', 'destroy')->middleware('auth:sanctum');
 });
-Route::middleware('auth:sanctum')->group(function () {
-    Route::controller(DeviceController::class)->prefix('/device')->group(function () {
-        Route::get('/index', 'index');
-        Route::get('/edit/{id}', 'edit');
-        Route::get('/get/{id}', 'getDevice');
-        Route::post('/store', 'store');
-        Route::get('/show/{id}', 'show');
-        Route::put('/update/{id}', 'update');
-        Route::delete('/delete/{id}', 'destroy');
-    });
+Route::controller(DeviceController::class)->prefix('/device')->group(function () {
+    Route::get('/index', 'index')->middleware('auth:sanctum');
+    Route::get('/edit/{id}', 'edit')->middleware('auth:sanctum');
+    Route::get('/get/{id}', 'getDevice');
+    Route::post('/store', 'store')->middleware('auth:sanctum');
+    Route::get('/show/{id}', 'show')->middleware('auth:sanctum');
+    Route::put('/update/{id}', 'update')->middleware('auth:sanctum');
+    Route::delete('/delete/{id}', 'destroy')->middleware('auth:sanctum');
 });
