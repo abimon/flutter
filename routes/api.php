214ec232baa26ee0b0149c232db771d7a2610cf3
abimon <eddyabby59@gmail.com>
2024-11-27 14:25:40 +0300

    updatedustbin

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ Route::controller(DustbinController::class)->prefix('/dustbin')->group(function 
     Route::get('/edit/{id}', 'edit');
     Route::post('/store', 'store');
     Route::get('/show/{id}', 'show')->middleware('auth:sanctum');
-    Route::put('/update/{dustbin_no}/{level}', 'update');
+    Route::post('/update/{dustbin_no}/{level}', 'update');
     Route::delete('/delete/{id}', 'destroy')->middleware('auth:sanctum');
 });
 Route::controller(PickupController::class)->prefix('/pickup')->group(function () {
