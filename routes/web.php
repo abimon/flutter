34f39b4bcee69378075f1d54e6404394672c9cf6
abimon <eddyabby59@gmail.com>
2026-02-08 07:46:08 +0300

    recallibrate the dustbin

--- routes/web.php
+++ routes/web.php
@@ -4,18 +4,14 @@ use App\Http\Controllers\APIContoller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', [APIContoller::class,'index']);
 Auth::routes();
-Route::get('/chalk', function(){ return view('chalk');});
-Route::get('/treasure', function(){ return view('treasure');});
+Route::get('/chalk', function () {
+    return view('chalk');
+});
+Route::get('/treasure', function () {
+    return view('treasure');
+});
+Route::get('/masterguide', function () {
+    return view('masterguide');
+});
